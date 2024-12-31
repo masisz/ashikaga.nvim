bf095614da66ca452102fce8a5f4b36e807b657b
masisz <iso.masatoshi@gmail.com>
2024-12-31 17:47:29 +0900

    I added a configuration to markdown information, todo

--- lua/ashikaga/colors/treesitter.lua
+++ lua/ashikaga/colors/treesitter.lua
@@ -4,7 +4,7 @@ function M.setup()
 	local util = require("ashikaga.lib.utils")
 	local color = require("ashikaga.lib.base_color")
 
-	-- code highlight
+	-- code
 	util.highlight("@field", { fg = color.ask.omugi_gold })
 	util.highlight("@property", { fg = color.ask.sky })
 	util.highlight("@keyword", { fg = color.ask.flower_fuji })
@@ -19,11 +19,28 @@ function M.setup()
 	util.highlight("@variable.member", { fg = color.ask.sky })
 	util.highlight("@punctuation.bracket", { fg = color.ask.gray })
 
+	local todo = { fg = color.ask.white, bg = color.ask.watarase_blue }
+	local note = { fg = color.ask.white, bg = color.ask.flower_fuji }
+	local warning = { fg = color.ask.white, bg = color.ask.omugi_gold }
+	local error = { fg = color.ask.white, bg = color.ask.orihime_red }
+
+	-- comment
+	util.highlight("@comment.todo", todo)
+	util.highlight("@comment.note", note)
+	util.highlight("@comment.warning", warning)
+	util.highlight("@comment.error", error)
+
+	-- todo
+	util.highlight("@text.todo", todo)
+	util.highlight("@text.note", note)
+	util.highlight("@text.warning", warning)
+	util.highlight("@text.danger", error)
+
 	-- markdown
 	util.highlight("@markup.heading.1.markdown", { fg = color.ask.orihime_red, bold = true })
 	util.highlight("@markup.heading.2.markdown", { fg = color.ask.flower_fuji, bold = true })
 	util.highlight("@markup.heading.3.markdown", { fg = color.ask.omugi_gold, bold = true })
-	util.highlight("@markup.heading.4.markdown", { fg = color.ask.white, bold = true })
+	util.highlight("@markup.heading.4.markdown", { fg = color.ask.icho_green, bold = true })
 	util.highlight("@markup.heading.5.markdown", { fg = color.ask.white, bold = true })
 	util.highlight("@markup.heading.6.markdown", { fg = color.ask.white, bold = true })
 
@@ -33,6 +50,11 @@ function M.setup()
 	util.highlight("@markup.list.checked", { fg = color.ask.icho_green })
 	util.highlight("@markup.list.unchecked", { fg = color.ask.icho_green })
 	util.highlight("@markup.link", { fg = color.ask.flower_fuji })
+
+	-- c/cpp
+	util.highlight("@type.builtin.c", { fg = color.ask.flower_fuji })
+	util.highlight("@type.builtin.cpp", { fg = color.ask.flower_fuji })
+	util.highlight("@property.cpp", { fg = color.ask.watarase_blue })
 end
 
 return M
